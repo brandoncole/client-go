792748b5e7faa942793da439ad859e08a6bfc399
Brandon Cole <brandon.w.cole@gmail.com>
2021-05-16 13:38:32 -0400

    docs: update misspelled word in comment
    Corrected 'imperonate' to 'impersonate'

--- tools/clientcmd/api/types.go
+++ tools/clientcmd/api/types.go
@@ -124,7 +124,7 @@ type AuthInfo struct {
 	// Impersonate is the username to act-as.
 	// +optional
 	Impersonate string `json:"act-as,omitempty"`
-	// ImpersonateGroups is the groups to imperonate.
+	// ImpersonateGroups is the groups to impersonate.
 	// +optional
 	ImpersonateGroups []string `json:"act-as-groups,omitempty"`
 	// ImpersonateUserExtra contains additional information for impersonated user.
